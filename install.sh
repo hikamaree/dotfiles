319714c44e24ccf5bddd25455e455f8a1a007be2
hikamaree <marko.r463@gmail.com>
2023-08-10 11:15:47 +0200

    update

--- install.sh
+++ install.sh
@@ -7,15 +7,39 @@ cp -r local ~/.local
 mv ~/.librewolf ~/.old_librewolf
 cp -r librewolf ~/.librewolf
 
+mkdir -p ~/Documents
+mkdir -p ~/Downloads
+mkdir -p ~/Music
+mkdir -p ~/Pictures/screenshots
+
+sudo ./system.sh $USER
+
 echo "Is this laptop? (Y/N): "
 read LAPTOP
 
 git clone https://aur.archlinux.org/yay.git
-
 cd yay
 makepkg -si
 cd ..
-yay -Syu
+yay --noconfirm -Syu
+
+# packages
+yay -S --noconfirm xorg-xinit xorg-server xorg-xset libxft libx11 libxinerama webkit2gtk vim zsh htop neofetch lf ueberzugpp maim xclip ncmpcpp mpd mpv feh picom orchis-theme librewolf discord steam pinta minecraft-launcher
+
+git clone https://github.com/hikamaree/suckless.git
+cd suckless/dwm
+sudo make clean install
+cd ../dwmblocks
+sudo make clean install
+cd ../dmenu
+sudo make clean install
+cd ../st
+sudo make clean install
+cd ../..
+
+sudo cp res/20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
+
+sudo systemctl --global enable mpd
 
 # laptop
 if [[ $LAPTOP == 'y' || $LAPTOP == 'Y' ]]
@@ -26,23 +50,11 @@ then
     cd ..
     yay -S --noconfirm auto-cpufreq
     sudo systemctl enable auto-cpufreq
+    sudo cp res/30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 fi
-
-# packages
-git clone https://github.com/hikamaree/suckless.git
-
-yay -S --noconfirm xorg-xinit xorg-server xorg-xset libxft libx11 libxinerama webkit2gtk vim zsh htop neofetch lf ueberzugpp maim xclip ncmpcpp mpd mpv feh picom orchis-theme librewolf-bin discord steam pinta minecraft-launcher
 
 yay --noconfirm -Yc
 
-# librewolf
-
-# system
 chsh -s /bin/zsh
-mkdir -p ~/Documents
-mkdir -p ~/Downloads
-mkdir -p ~/Music
-mkdir -p ~/Pictures/screenshots
-sudo ./system.sh $USER
 
 reboot
--- system.sh
+++ system.sh
@@ -1,18 +1,3 @@
-cd suckless/dwm
-make clean install
-
-cd ../dwmblocks
-make clean install
-
-cd ../dmenu
-make clean install
-
-cd ../st
-make clean install
-
-cd ../..
-
-cp res/20-amdgpu.conf /etc/X11/xorg.conf.d/20-amdgpu.conf
-
+echo -e "MAKEFLAGS=\"-j16\"" >> etc/makepkg.conf
 echo "ZDOTDIR=/home/$1/.config/zsh" >> /etc/environment
 echo "source /home/$1/.config/vim/vimrc" >> /etc/vimrc
