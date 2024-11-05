0fa07de5ff97b395cf0575fea075f4f716bcdae4
Marko Radosavljevic <marko.r463@gmail.com>
2024-11-05 21:36:14 +0100

    update

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -2,16 +2,21 @@ vim.keymap.set({"n", "v", "i", "t"}, "<C-Q>", function() vim.cmd([[wa!]]) vim.cm
 vim.keymap.set('n', "<Esc>", function() vim.cmd([[nohlsearch]]) end)
 vim.keymap.set("x", "p", [["_dP]])
 
-vim.keymap.set("n", "<C-c>", function() vim.cmd([[w!]]) vim.cmd([[bd!]]) vim.cmd([[bnext]]) end)
-vim.keymap.set({"n", "t"}, "<C-Left>", function() vim.cmd([[bprev]]) end)
-vim.keymap.set({"n", "t"}, "<C-Right>", function() vim.cmd([[bnext]]) end)
-vim.keymap.set("n", "<C-Down>", function() pcall(vim.cmd, [[cprev]]) end)
-vim.keymap.set("n", "<C-Up>", function() pcall(vim.cmd, [[cnext]]) end)
+vim.keymap.set("n", "<C-c>", function()  pcall(vim.cmd, [[w!]]) vim.cmd([[bd!]]) vim.cmd([[bnext]]) end)
+vim.keymap.set({"n", "t"}, "<C-,>", function() vim.cmd([[bprev]]) end)
+vim.keymap.set({"n", "t"}, "<C-.>", function() vim.cmd([[bnext]]) end)
+vim.keymap.set("n", "<C-S-,>", function() pcall(vim.cmd, [[cprev]]) end)
+vim.keymap.set("n", "<C-S-.>", function() pcall(vim.cmd, [[cnext]]) end)
 
-vim.keymap.set({"n", "t"}, "<S-Left>", function() vim.cmd([[wincmd h]]) end)
-vim.keymap.set({"n", "t"}, "<S-Right>", function() vim.cmd([[wincmd l]]) end)
-vim.keymap.set({"n", "t"}, "<S-Down>", function() vim.cmd([[wincmd j]]) end)
-vim.keymap.set({"n", "t"}, "<S-Up>", function() vim.cmd([[wincmd k]]) end)
+vim.keymap.set({"n", "t"}, "<C-Left>", function() vim.cmd([[wincmd h]]) end)
+vim.keymap.set({"n", "t"}, "<C-Right>", function() vim.cmd([[wincmd l]]) end)
+vim.keymap.set({"n", "t"}, "<C-Down>", function() vim.cmd([[wincmd j]]) end)
+vim.keymap.set({"n", "t"}, "<C-Up>", function() vim.cmd([[wincmd k]]) end)
+
+vim.keymap.set({"n", "t"}, "<C-S-Left>", function() vim.cmd([[vert res -2]]) end)
+vim.keymap.set({"n", "t"}, "<C-S-Right>", function() vim.cmd([[vert res +2]]) end)
+vim.keymap.set({"n", "t"}, "<C-S-Up>", function() vim.cmd([[hor res -1]]) end)
+vim.keymap.set({"n", "t"}, "<C-S-Down>", function() vim.cmd([[hor res +1]]) end)
 
 vim.keymap.set("x", "<A-Up>", [[:move '<-2<CR>gv-gv]])
 vim.keymap.set("x", "<A-Down>", [[:move '>+1<CR>gv-gv]])
@@ -23,9 +28,9 @@ vim.keymap.set("n", "<C-\\>", vim.diagnostic.open_float)
 vim.keymap.set("n", "<C-[>", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "<C-]>", vim.diagnostic.goto_next)
 
-vim.keymap.set("t", "<C-T>", function() vim.cmd([[bd!]]) end)
+vim.keymap.set("t", "<C-Enter>", function() vim.cmd([[bd!]]) end)
 vim.keymap.set("t", "<Esc>", function() vim.cmd([[stopinsert]]) end)
-vim.keymap.set("n", "<C-T>", function()
+vim.keymap.set("n", "<C-Enter>", function()
 	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 		if vim.bo[buf].buftype == "terminal" then
 			vim.api.nvim_buf_delete(buf, {force = true})
